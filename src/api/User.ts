3099944cdf3e8401588c0114528732531a1fc540
imgiang <hoahonggai1199@gmail.com>
2022-03-25 07:33:31 +0700

    user.ts

--- src/api/User.ts
+++ src/api/User.ts
@@ -3,9 +3,9 @@ import { User } from '@/models/User'
 
 const apiUrl = 'users'
 
-export const getUserById = async (id: string): Promise<User> => {
+export const getUserById = async (email: string): Promise<User> => {
     try {
-        const res = await axios.get(`${apiUrl}/${id}`)
+        const res = await axios.get(`${apiUrl}/${email}`)
         return res.data
     } catch (error) {
         throw new Error('Failed to get user')
